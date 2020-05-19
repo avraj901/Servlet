ffca2d97a2f92242c7e3575fedab2b56e5938786
avraj901 <avraj901@gmail.com>
2020-05-19 10:57:34 -0400

    modified Second Example some mistake in second Example

---
+++ servletSecondExample/MIMEApp/WEB-INF/classes/PlainServlet.java
@@ -0,0 +1,34 @@
+package com.nt.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class PlainServlet extends HttpServlet{
+
+      protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException , IOException{
+                
+            PrintWriter pw=null;
+               //get PrintWriter.
+               pw=res.getWriter();
+               //set Response Content Type/MIME Type
+               res.setContentType("text/plain");
+               //Write output message to res object.
+                pw.println("<table align='center' border='1' color='cyan'>");
+                pw.println("<tr><th>country</th><th>President</th>");
+                pw.println("<tr><td>India</td><td>Modi</td></tr>");
+                pw.println("<tr><td>Pakistan</td><td>ImranKhan</td></tr>");
+                pw.println("<tr><td>Us</td><td>Trump</td></tr>");
+                pw.println("<tr><td>China</td><td>Xi-xing ping</td></tr>");
+                pw.println("</table>");
+
+                //close Stream
+                
+                pw.close();
+                 
+
+    }
+
+}
+
+
